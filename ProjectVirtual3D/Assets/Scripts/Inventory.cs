619a6524b69956a3d0c859fd6bfec5629664de33
Robrecht Rikken <robin.rikken@gmail.com>
2017-11-24 02:47:42 +0100

    Adds Inventory coll and button response to open the inventory
    There now is a collider on the left controller to put items in that you want to add to your inventory. You can open this by pressing the top half of the controller. You can press the bottom half of the touchpad to open the inventory.

---
+++ ProjectVirtual3D/Assets/Scripts/Inventory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Inventory : MonoBehaviour {
+	//List to hold all inventory objects
+	private List<GameObject> InventoryStorage;
+	public GameObject inventorySphere;
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<VRTK_ControllerEvents>().TouchpadPressed += new ControllerInteractionEventHandler(ShowInventoryOrSphere);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void AddItemToInventory(GameObject itemtoAdd)
+	{
+		//Adds the item to the InventoryStorage List and removes it from the scene
+		InventoryStorage.Add(itemtoAdd);
+		Destroy (itemtoAdd);
+	}
+
+	public void ShowInventoryOrSphere(object sender, ControllerInteractionEventArgs e)
+	{
+		//When the touchpad is clicked on this controller (inventory controller) check if the bottom or top half is clicked
+		//Top half will result in the enabling of your inventory shpere which will allow the adding of items to the inventory
+		if (e.touchpadAxis.y > 0.1) 
+		{
+			Debug.Log ("Top of touchpad of " + this.name + " is clicked");
+			if (inventorySphere.activeSelf) 
+			{
+				inventorySphere.SetActive (false);
+			} 
+			else 
+			{
+				inventorySphere.SetActive (true);				
+			}
+		}
+		//Bottom half of the touchpad will show your inventory with all the items added inside
+		if (e.touchpadAxis.y < -0.1) 
+		{
+			Debug.Log ("Bottom of touchpad of " + this.name + " is clicked");
+		}
+
+	}
+}
