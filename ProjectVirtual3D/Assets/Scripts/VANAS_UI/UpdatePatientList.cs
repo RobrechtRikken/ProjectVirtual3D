1117dd13777e7ffe051ef881a170938cb7a139ec
Unknown <stijn.g1991@gmail.com>
2017-12-04 21:39:03 +0100

    Adds VANAS Patient UI
    Adds VANAS Patient UI in a seperate UI test scene

---
+++ ProjectVirtual3D/Assets/Scripts/VANAS_UI/UpdatePatientList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdatePatientList : MonoBehaviour
+{
+
+	public LoadPatientsList patientList;
+	public Transform patienlistParent;
+
+	public void ShowPatient(Text inputText)
+	{
+		Debug.Log("Update list");
+
+
+		Transform[] allChildren = GetComponentsInChildren<Transform>();
+		foreach (Transform child in allChildren)
+		{
+			// do whatever with child transform here
+			if (child.transform.GetChild(0).GetComponent<Text>().text.Contains(inputText.text))
+			{
+				child.transform.gameObject.SetActive(true);
+			}
+			else
+			{
+				child.transform.gameObject.SetActive(false);
+			}
+		}
+	}
+}
