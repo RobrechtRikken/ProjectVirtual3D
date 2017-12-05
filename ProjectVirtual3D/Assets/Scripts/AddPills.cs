3be5709d3252c2a34ce86f98134572d7bc70e143
Robrecht Rikken <robin.rikken@gmail.com>
2017-12-05 01:40:33 +0100

    Creates script so vanas cabinet can be instantiated with pills inside
    A script is made where you can choose which itemDrop zone will have a pill already in it on start.

---
+++ ProjectVirtual3D/Assets/Scripts/AddPills.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class AddPills : MonoBehaviour {
+
+	public VRTK_SnapDropZone[] dropZoneList;
+	public GameObject pillPrefab;
+	// Use this for initialization
+	void Start () {
+		foreach(VRTK_SnapDropZone oneDropZone in dropZoneList)
+		{
+			GameObject newPill = Instantiate (pillPrefab,oneDropZone.transform.position, oneDropZone.transform.rotation);
+			oneDropZone.ForceSnap (newPill);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
