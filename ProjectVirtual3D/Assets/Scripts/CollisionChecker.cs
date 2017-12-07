73367e2da77c82287fb2e0a92b237ba5991a0076
Robrecht Rikken <robin.rikken@gmail.com>
2017-12-08 00:29:10 +0100

    Adds Post Processing Stack & new working doors & prefabs
    Doors work again with new VRTK system. New prefabs made

---
+++ ProjectVirtual3D/Assets/Scripts/CollisionChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionChecker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OnCollisionEnter(Collision col)
+	{
+		Debug.Log ("this is colliding with " + this.gameObject.name + " : " + col.gameObject.name);
+	}
+}
