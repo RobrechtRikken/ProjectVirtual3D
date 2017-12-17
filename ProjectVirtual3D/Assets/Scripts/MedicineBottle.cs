29a1f50c6b3511ebcda4e2f981e3d9112a6937cf
Robrecht Rikken <robin.rikken@gmail.com>
2017-12-17 17:44:33 +0100

    Adds cycling through the amount of medicine you want from the bottle
    You can now cycle through the amount of medicine you want to get from a bottle when you pick it up and press the use/trigger button.
    Method made for cycling and a method made for extracting the liquid that will be called from the syringe.

---
+++ ProjectVirtual3D/Assets/Scripts/MedicineBottle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class MedicineBottle : MonoBehaviour {
+
+	public float amountInBottle = 5f;
+	public float selectedAmount = 0f;
+	private float maxAmount;
+	private float minamount = 0f;
+	// Use this for initialization
+	void Start () 
+	{
+		maxAmount = amountInBottle;		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CycleAmountToUse()
+	{
+		//Cycle through the amount of cc you want to take out of the bottle
+		Debug.Log (this.name + " is cycling through amounts");
+		if (selectedAmount == maxAmount) {
+			selectedAmount = minamount;
+		}
+		else 
+		{
+			selectedAmount++;
+		}
+	}
+
+	public void RemoveSelectedAmount()
+	{
+		//remove the selected amount from the bottle
+		amountInBottle = amountInBottle - selectedAmount;
+		//Reset the selected amount
+		selectedAmount = minamount;
+		//Set the maxAmount for the bottle to what is left in the bottle
+		maxAmount = amountInBottle;
+	}
+}
