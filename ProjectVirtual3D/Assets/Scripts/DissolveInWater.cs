611ec377033fae7c7cc769c4098f8e355fe79006
Robrecht Rikken <robin.rikken@gmail.com>
2017-12-18 13:37:05 +0100

    Adds dissolvable pill in water

---
+++ ProjectVirtual3D/Assets/Scripts/DissolveInWater.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveInWater : MonoBehaviour {
+
+	public string dissolvableMedicineTag = "Dissolve";
+	public Material fizzyMaterial;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == dissolvableMedicineTag) 
+		{
+			//Medicine can be dissolved remove it from scene
+			Destroy(col.gameObject);
+			Debug.Log ("Change texture of water when dissolved");
+			this.GetComponent<MeshRenderer> ().material = fizzyMaterial;
+		}
+	}
+}
