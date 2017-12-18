68e016e43ae570b5c9b8e9a72e6380af39f4bc6c
Robrecht Rikken <robin.rikken@gmail.com>
2017-12-19 00:40:01 +0100

    Adds ui pointers (lines) to both controllers
    You now get a ui pointer when hovering over ui. Left controller has one. Right controller replaces teleport pointer with line pointer when using UI

---
+++ ProjectVirtual3D/Assets/Scripts/UIPointerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class UIPointerController : MonoBehaviour {
+
+	public VRTK_Pointer dePointer;
+	// Use this for initialization
+	void Start () 
+	{
+		dePointer = this.GetComponent<VRTK_Pointer> ();
+	}
+
+
+	public void whenPointerEntersUI()
+	{
+		Debug.Log ("Entered a UI canvas");
+		dePointer.enabled = true;
+	}
+
+	public void whenPointerLeavesUI()
+	{
+		Debug.Log ("Left a Ui canvas");
+		dePointer.enabled = !dePointer.enabled;
+	}
+}
---
+++ ProjectVirtual3D/Assets/Scripts/UIPointerControllerRight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class UIPointerControllerRight : MonoBehaviour {
+
+	public VRTK_Pointer BezierPointer;
+	public VRTK_Pointer UIPointer;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+
+	public void whenPointerEntersUI()
+	{
+		Debug.Log ("Entered a UI canvas");
+		BezierPointer.enabled = !BezierPointer.enabled;
+		UIPointer.enabled = !UIPointer.enabled;
+	}
+
+	public void whenPointerLeavesUI()
+	{
+		Debug.Log ("Left a Ui canvas");
+		BezierPointer.enabled = !BezierPointer.enabled;
+		UIPointer.enabled = !UIPointer.enabled;
+	}
+}
