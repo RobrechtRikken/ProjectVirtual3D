7d8201dd6cb9b03282ca5508a8e7f4b433ef26c2
Robrecht Rikken <robin.rikken@gmail.com>
2018-01-20 21:57:41 +0100

    Adds final door script open on click

--- ProjectVirtual3D/Assets/Scripts/DoorV2.cs
+++ ProjectVirtual3D/Assets/Scripts/DoorV2.cs
@@ -5,23 +5,63 @@ using UnityEngine;
 public class DoorV2 : MonoBehaviour 
 {
 	public bool doorOpen = false;
-	public float rotationSpeed = 1f;
+	public float rotationTime;
+	private Quaternion openRotation;
+	private Quaternion closedRotation;
+	public float openValue = 0.95f;
+	private bool doorMoving = false;
+	public bool inwardOutward = true;
 
+	 void Start()
+	{
+		closedRotation = this.transform.rotation;
+		openRotation = closedRotation;
+		if (inwardOutward) {
+			openRotation.y += openValue;
+		} 
+		else 
+		{
+			openRotation.y -= openValue;
+		}
+
+		rotationTime = 2f;
+	}
 	public void ToggleDoor()
 	{
 		Debug.Log ("Door is used");
-		if (!doorOpen) 
+		if (!doorOpen && !doorMoving) 
 		{
 			Debug.Log ("Door is now open");
-			transform.Rotate (Vector3.up * (rotationSpeed * Time.deltaTime));
+			//transform.Rotate (Vector3.up * (rotationSpeed * Time.deltaTime));
+			StartCoroutine(RotateOverTime(closedRotation, openRotation, rotationTime));
 			doorOpen = true;
 		}
 		else 
 		{
 			Debug.Log ("Door is now closed");
-			transform.Rotate (-Vector3.up * (rotationSpeed * Time.deltaTime));
+			//transform.Rotate (-Vector3.up * (rotationSpeed * Time.deltaTime));
+			StartCoroutine(RotateOverTime(openRotation, closedRotation, rotationTime));
 			doorOpen = false;
 		}
+	}
+
+	IEnumerator RotateOverTime (Quaternion originalRotation, Quaternion finalRotation, float duration) {
+		doorMoving = true;
+		if (duration > 0f) {
+			float startTime = Time.time;
+			float endTime = startTime + duration;
+			this.transform.rotation = originalRotation;
+			yield return null;
+			while (Time.time < endTime) 
+			{
+				float progress = (Time.time - startTime) / duration;
+				// progress will equal 0 at startTime, 1 at endTime.
+				this.transform.rotation = Quaternion.Slerp (originalRotation, finalRotation, progress);
+				yield return null;
+			}
+		}
+			this.transform.rotation = finalRotation;
+			doorMoving = false;
 	}
 
 }
