ba96d4f4b878b7a2f7d67501a84fc422809a31a6
Unknown <stijn.g1991@gmail.com>
2018-01-21 00:18:29 +0100

    Adds feedback to UI drawer unlock
    Adds feedback to the UI when a drawer is unlocked

---
+++ ProjectVirtual3D/Assets/Scripts/VANAS_UI/Blink.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour {
+	[SerializeField]
+	private float timeToBlink = 3f; //blink the "check to your left" text for X seconds when entering
+	public float blinkIntervals = 0.5f;
+	[SerializeField]
+	private GameObject blinkObject;
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(BlinkOnOff(timeToBlink));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator BlinkOnOff(float _waitTime)
+	{
+		float endTime = Time.time + _waitTime;
+		while (Time.time < endTime)
+		{
+			blinkObject.SetActive(true);
+			yield return new WaitForSeconds(blinkIntervals);
+			blinkObject.SetActive(false);
+			yield return new WaitForSeconds(blinkIntervals);
+
+		}
+	}
+}
