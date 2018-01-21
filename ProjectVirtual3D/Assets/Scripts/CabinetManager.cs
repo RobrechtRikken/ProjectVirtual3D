b7c71a0eb198f92262264e91bff4c4c3fb58b878
Robrecht Rikken <robin.rikken@gmail.com>
2018-01-21 21:47:09 +0100

    Removes redundant scripts from drawers, cabinetmanager added
    Cabinetmanager lists all cabinets

--- ProjectVirtual3D/Assets/Scripts/CabinetManager.cs
+++ ProjectVirtual3D/Assets/Scripts/CabinetManager.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CabinetManager : MonoBehaviour {
-
+	private int amountOfCabinets = 5;
+	public string drawerTag = "Drawer";
+	public string drawerSlotTag = "DrawerSlot";
+	public string cabinetTag = "Cabinet";
+	public List<GameObject> cabinetList = new List<GameObject>();
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{	
+		for (int i = 0; i < amountOfCabinets; i++) 
+		{
+			cabinetList.Add(GameObject.FindGameObjectWithTag(cabinetTag + i.ToString()));
+			Debug.Log (GameObject.FindGameObjectWithTag (cabinetTag + i.ToString ()).name + " added to cabinet list");
+		}
+
 	}
 	
 	// Update is called once per frame
