70e775531bf00713ca4e3cd5eed02ed93b87ff56
Robrecht Rikken <robin.rikken@gmail.com>
2018-01-24 17:10:01 +0100

    Adds toggle button for the browser

---
+++ ProjectVirtual3D/Assets/Scripts/BrowserToggle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrowserToggle : MonoBehaviour {
+
+	public GameObject theBrowser;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ToggleBrowser()
+	{
+		if (theBrowser.gameObject.activeSelf) 
+		{
+			theBrowser.gameObject.SetActive (false);
+		}
+		else
+		{
+			theBrowser.gameObject.SetActive(true);
+		}
+	}
+}
