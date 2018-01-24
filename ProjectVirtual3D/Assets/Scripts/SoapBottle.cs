0382caa8cb9bc364a847bec170e9121fd84cee4d
Robrecht Rikken <robin.rikken@gmail.com>
2018-01-24 04:30:34 +0100

    Adds sparkle SOAP && needs fix only sparkles when held

---
+++ ProjectVirtual3D/Assets/Scripts/SoapBottle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoapBottle : MonoBehaviour {
+
+	public ParticleSystem theSparkle = new ParticleSystem ();
+	// Use this for initialization
+	void Start () 
+	{
+		theSparkle = GetComponentInChildren<ParticleSystem>();
+		theSparkle.Stop ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SanitiseHands()
+	{
+		SoundManager.instance.PlaySoundHover("SparkleII");
+		theSparkle.Stop ();
+		theSparkle.Play ();
+		//theSparkle.gameObject.SetActive (true);
+	}
+}
